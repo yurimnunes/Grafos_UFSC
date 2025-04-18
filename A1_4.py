5bea1da5246d05677806c5dbb79e77fbe9b23865
Mariana <62575934+ma-riana@users.noreply.github.com>
2025-04-18 20:07:21 -0300

    Bellman-Ford

---
+++ A1_4.py
@@ -0,0 +1,33 @@
+def bellman_ford(g, s, w):
+    dist = []
+    pred = []
+
+    for v in range(g.qtdVertices() + 1):
+        dist.append(float("inf"))  # usar float infinito
+        pred.append(None)  # usar None como nulo
+    dist[s] = 0
+
+    # print(dist)
+    # print(pred)
+
+    i = 1
+    while i < g.qtdVertices():
+        for u, v in g.arestas:
+            if dist[u] + w[u - 1][v - 1] < dist[v]:
+                dist[v] = dist[u] + w[u - 1][v - 1]
+                pred[v] = u
+        i += 1
+
+    for u, v in g.arestas:
+        if dist[u] + w[u - 1][v - 1] < dist[v]:
+            print("Tem ciclo negativo.")
+
+    for v in g.vertices:
+        pred_list = []
+        if dist[v] != float("inf"):
+            u = v
+            while u is not None:
+                pred_list.insert(0, u)
+                u = pred[u]
+        lista_str = ",".join(map(str, pred_list))
+        print(f"{v}: {lista_str}; d= {dist[v]}")
